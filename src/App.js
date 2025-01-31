5caed6a5c3ca837081830dd39220bf33288d8644
dipaligaikwad12 <gdipali070@gmail.com>
2025-01-31 20:46:45 +0530

    city app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import "./App.css"
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <h1 className='text-red-800 text-6xl text-center'>Ahilyanagar</h1>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoPO_p7U7IWeba0-JXzZK7cc6atMI2uhV7Kw&s" className='h-74 rounded-xl m-auto mt-8'/>
+      <p className='mt-8 mx-32 text-xl '>Ahmednagar, also known as Ahilyanagar, is a city in the state of Maharashtra, India. It is the headquarters of the Ahmednagar district. Located along the banks of the Sina River, Ahmednagar is a city rich in historical significance and offers a wealth of sights including ancient monuments, majestic palaces, and intriguing museums.Ahmednagar is a relatively small town and shows less development than the nearby western Maharashtra cities of Mumbai and Pune.  Marathi is the primary language for daily-life communication.</p>
+    </div>
   )
 }
 
